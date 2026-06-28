0d383b365e7c7943967eda70dfaf5e0ca74b9be0
Sanjusri <kit28.24bad140@gmail.com>
2026-06-28 22:00:09 +0530

    Delete lib directory

--- lib/models/app_setting.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppSettings {
-  static Color primaryColor = const Color(0xFF2563EB);
-  static bool darkMode = false;
-}
--- lib/models/user_data.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:typed_data';
-
-class UserData {
-  static String name = "Learner";
-  static String email = "learner@example.com";
-  static String phone = "+91 9876543210";
-  static Uint8List? profileImage;
-}
